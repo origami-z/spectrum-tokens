7c1aee860837937c994e21dea62d6110d5c839b3
garthdb <garthdb@gmail.com>
2023-09-06 17:08:19 -0600

    test(tokens): added tests for json schema type validators

---
+++ packages/tokens/test/schemaValidators/alias.test.js
@@ -0,0 +1,41 @@
+/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import test from "ava";
+import Ajv from "ajv/dist/2020.js";
+import addFormats from "ajv-formats";
+import { readFile } from "fs/promises";
+
+const readJSON = async (filePath) =>
+  JSON.parse(await readFile(filePath, "utf8"));
+
+const ajv = new Ajv();
+addFormats(ajv);
+
+ajv.addSchema(await readJSON("schemas/token-types/token.json"));
+const validate = await ajv.compile(
+  await readJSON("schemas/token-types/alias.json"),
+);
+
+test("Every token schema should validate against the definition", (t) => {
+  const alias = {
+    component: "swatch",
+    $schema:
+      "https://opensource.adobe.com/spectrum-tokens/schemas/token-types/alias.json",
+    value: "{gray-900}",
+    uuid: "7da5157d-7f25-405b-8de0-f3669565fb48",
+  };
+  if (!validate(alias)) {
+    console.log(validate.errors);
+  }
+  t.pass();
+});
---
+++ packages/tokens/test/tokenSchemaValidator.test.js
@@ -0,0 +1,84 @@
+/*
+Copyright 2023 Adobe. All rights reserved.
+This file is licensed to you under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License. You may obtain a copy
+of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software distributed under
+the License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR REPRESENTATIONS
+OF ANY KIND, either express or implied. See the License for the specific language
+governing permissions and limitations under the License.
+*/
+
+import test from "ava";
+import Ajv from "ajv/dist/2020.js";
+import addFormats from "ajv-formats";
+import { glob } from "glob";
+import { readFile } from "fs/promises";
+
+const readJSON = async (filePath) =>
+  JSON.parse(await readFile(filePath, "utf8"));
+
+const ajv = new Ajv();
+addFormats(ajv);
+
+test.before(async (t) => {
+  const schemaFileNames = await glob("schemas/token-types/*.json");
+  const schemaIds = await Promise.all(
+    schemaFileNames.map(async (fileName) => {
+      const json = await readJSON(fileName);
+      try {
+        ajv.addSchema(json, json["$id"]);
+      } catch (error) {
+        console.log(error);
+        console.log(`${fileName} error with $id ${json["$id"]}`);
+      }
+      return json["$id"];
+    }),
+  );
+
+  const tokenFilesNames = await glob("src/*.json");
+  t.context.tokenFiles = await Promise.all(
+    tokenFilesNames.map(async (tokenFile) => {
+      return { fileName: tokenFile, data: await readJSON(tokenFile) };
+    }),
+  );
+
+  const validate = await ajv.compile({
+    $schema: "https://json-schema.org/draft/2020-12/schema",
+    type: "object",
+    patternProperties: {
+      "^(?:(?:[a-z]|\\d)+-?)*(?:[a-z]|\\d)+$": {
+        anyOf: schemaIds.map((schemaId) => ({ $ref: schemaId })),
+      },
+    },
+  });
+});
+
+test("Every token should have a $schema property", (t) => {
+  t.context.tokenFiles.forEach((tokenFileObj) => {
+    Object.keys(tokenFileObj.data).forEach((tokenName) => {
+      t.true(
+        Object.hasOwn(tokenFileObj.data[tokenName], "$schema"),
+        `${tokenName} in ${tokenFileObj.fileName} does not have a $schema property.`,
+      );
+    });
+  });
+});
+
+test("Every token schema should validate against the definition", (t) => {
+  t.context.tokenFiles.forEach((tokenFileObj) => {
+    Object.keys(tokenFileObj.data).forEach((tokenName) => {
+      ajv.validate(
+        tokenFileObj.data[tokenName]["$schema"],
+        tokenFileObj.data[tokenName],
+      );
+      t.is(
+        ajv.errors,
+        null,
+        `${tokenName} in ${tokenFileObj.fileName} failed validation.`,
+      );
+    });
+  });
+  t.log("All tokens validated against their schemas.");
+});
